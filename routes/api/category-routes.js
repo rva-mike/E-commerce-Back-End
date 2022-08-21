b2cdc952d50133ebb761c4f3b54ef223dace3d75
Mike L <mike.levy.rva@gmail.com>
2022-08-21 14:20:27 -0400

    added get route to find 1 category by id, added if statement to .then

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,17 +8,22 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
   Category.findAll({
     attributes: ['id', 'category_name'],
-    include: [
-        {
-            model: Product,
-            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-        }
-    ]
-})
-    .then(dbCategoryData => res.json(dbCategoryData))
+    include: 
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No categories found.' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err)
     });
 });
 
@@ -28,18 +33,52 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include:
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id.' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+
+
 
 router.post('/', (req, res) => {
   // create a new category
 });
 
+
+
+
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
+
+
+
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
+
+
+
 
 module.exports = router;
