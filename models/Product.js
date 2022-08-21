51d3a56c52ae57b58514df02d6201a8ed76736e8
Mike L <mike.levy.rva@gmail.com>
2022-08-21 13:53:11 -0400

    added validations to price and stock

--- models/Product.js
+++ models/Product.js
@@ -23,12 +23,18 @@ Product.init(
       type: DataTypes.DECIMAL,
       allowNull: false,
       isDecimal: true,
+      validate: {
+        isDecimal: true, 
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       isNumeric: true,
+      Validate: {
+        isNumeric: true, 
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
